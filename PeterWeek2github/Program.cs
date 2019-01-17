824e10cfeeba49bce861c0a3107c3ac37125cfa8
visualstudio <visualstudio@230-PC36>
2019-01-17 15:30:48 -0500

    neew change made

--- PeterWeek2github/Program.cs
+++ PeterWeek2github/Program.cs
@@ -1,12 +1,38 @@
 ﻿using System;
 
-namespace PeterWeek2github
+namespace peter_Week2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var red = new Method("roy", 17);
+            Console.WriteLine(red.AddNumbers());
+            Console.ReadLine();
+        }
+    }
+
+    class Method
+    {
+        public int AddNumbers()
+        {
+            int a = 0;
+            for (int i = 618; i >= 211; i = i - 18)
+            {
+                a = a + i;
+            }
+            Console.WriteLine("Sum of numbers between 618 to 211 that are deducted by 18 is " + a);
+            return a;
+        }
+
+        public Method()
+        {
+            Console.WriteLine("I am in the no ARGUMENTS. ");
+        }
+        public Method(string a, int b)
+        {
+            Console.WriteLine("{0},{1}", a, b);
+            Console.ReadLine();
         }
     }
 }
